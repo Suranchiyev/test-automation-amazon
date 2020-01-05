9818d976481824327fbc44d0362a5a0ffe843674
Munavvar Akabirova <munavvarakabirova@Munavvars-MBP.fios-router.home>
2020-01-04 23:50:29 -0500

    Java 8, chrome driver for mac

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SmokeTest {
     WebDriver driver;
+    public boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
 
     @BeforeEach
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        if(isWindows){
+            System.setProperty("webdriver.chrome.driver","resources/win/chromedriver.exe");
+        }else{
+            System.setProperty("webdriver.chrome.driver","resources/mac/chromedriver");
+        }
+
         driver = new ChromeDriver();
         driver.get("https://amazon.com");
     }
